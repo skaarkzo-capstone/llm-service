cb816f73f163c4e0a245c21adf0bd2f75426ac92
Arham Ansari <73043239+arhamansarii@users.noreply.github.com>
2024-11-16 02:44:37 -0500

    Update app/api/main/__init__.py
    Co-authored-by: Rakhshan Khan <81067904+rakhshankhan@users.noreply.github.com>

--- app/api/main/__init__.py
+++ app/api/main/__init__.py
@@ -2,4 +2,4 @@ from fastapi import APIRouter
 from app.api.main.controller import llm_controller
 
 api_router = APIRouter()
-api_router.include_router(llm_controller.router, prefix="/llm", tags=["example"])
+api_router.include_router(llm_controller.router)
