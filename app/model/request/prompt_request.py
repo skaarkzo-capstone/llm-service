1b82f201385bc9bdc35835f4e8c01b4813fb736a
Arham Ansari <73043239+arhamansarii@users.noreply.github.com>
2024-11-20 19:25:42 -0500

    Update prompt_request.py

--- app/model/request/prompt_request.py
+++ app/model/request/prompt_request.py
@@ -1,5 +1,21 @@
 from pydantic import BaseModel
+from typing import List
+
+class ProductDTO(BaseModel):
+    name: str
+    description: str
 
 # For request structure
 class PromptRequest(BaseModel):
-    input_text: str
+    id: int
+    name: str
+    industry: str
+    location: str
+    type: str  # "Public" or "Private"
+    size: str  # Number of employees or annual revenue
+    annual_revenue: float
+    market_cap: float
+    key_products: List[ProductDTO]
+    sustainability_goals: str
+    social_goals: str
+    governance: str
